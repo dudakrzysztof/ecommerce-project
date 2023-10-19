eaa9235bdde6e72683a2292c83a508904f56f5cb
KrzysztofDuda <duda_krzysztof@o2.pl>
2023-10-19 13:34:39 +0200

    Angular frontend - product list

--- 02-backend/spring-boot-ecommerce/src/main/java/io/github/dudakrzysztof/ecommerce/entity/Product.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/io/github/dudakrzysztof/ecommerce/entity/Product.java
@@ -40,7 +40,7 @@ public class Product {
     private boolean active;
 
     @Column(name = "units_in_stock")
-    private int unitInStock;
+    private int unitsInStock;
 
     @Column(name = "date_created")
     @CreationTimestamp

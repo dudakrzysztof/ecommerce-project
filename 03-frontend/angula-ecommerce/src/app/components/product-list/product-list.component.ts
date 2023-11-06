faf5c5a98ea51c3f898ff2326204060764f86562
KrzysztofDuda <duda_krzysztof@o2.pl>
2023-11-06 16:17:03 +0100

    Pagination

--- 03-frontend/angula-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angula-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,8 +12,16 @@ export class ProductListComponent {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   currentCategoryName: string = "";
   searchMode: boolean = false;
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElements: number = 0;
+  
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -39,13 +47,22 @@ export class ProductListComponent {
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
     this.currentCategoryName = this.route.snapshot.paramMap.get('keyword')!;
 
-    // now search for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+    // if we have a different keyword than previous
+    // then set thePageNumber to 1
 
+    if (this.previousKeyword != theKeyword){
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+    // now search for the products using keyword
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+                                               this.thePageSize,
+                                               theKeyword)
+                                                .subscribe(this.processResult());
   }
 
   handleListProducts() {
@@ -64,12 +81,41 @@ export class ProductListComponent {
       this.currentCategoryName = 'Books';
     }
 
+    //
+    // Check if we have a different category than previous
+    // Note: Angular will reuse a component if it is currently being vievied
+    //
+
+    // if we have a different category id than previous 
+    // then set thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber=1;
+    } 
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                               this.thePageSize,
+                                               this.currentCategoryId)
+                                               .subscribe(this.processResult());
+  }
+
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
+  processResult(){
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
   }
 
 }
--- 03-frontend/angula-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angula-ecommerce/src/app/services/product.service.ts
@@ -18,9 +18,20 @@ export class ProductService {
 
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    // need to build URL based on category id ... will come back to this!
+    // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
     return this.getProducts(searchUrl);
+  }
+
+  getProductListPaginate(thePage: number, 
+                         thePageSize: number, 
+                         theCategoryId: number): Observable<GetResponseProducts> {
+
+    // need to build URL based on category id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   searchProducts(theKeyword: string): Observable<Product[]> {
@@ -28,6 +39,18 @@ export class ProductService {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
     return this.getProducts(searchUrl);
   }
+
+  searchProductsPaginate(thePage: number, 
+                        thePageSize: number, 
+                        theKeyword: string): Observable<GetResponseProducts> {
+
+    // need to build URL based on keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+}
+  
 
   getProduct(theProductId: number): Observable<Product> {
     // need to build URL based on product id
@@ -55,6 +78,13 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+
   }
 }
 

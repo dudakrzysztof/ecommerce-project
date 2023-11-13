8ba06d371240d0c8ae8f1fd91896efd3fa9a2dcf
KrzysztofDuda <duda_krzysztof@o2.pl>
2023-11-13 19:21:45 +0100

    Checkout form drop-down lists for country and state implemented

--- 03-frontend/angula-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angula-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import {ShopFormService} from "../../services/shop-form.service";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +16,16 @@ export class CheckoutComponent implements OnInit{
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder){}
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = []
+  billingAddressStates: State[] = []
+
+  constructor(private formBuilder: FormBuilder,
+              private shopFormService: ShopFormService){}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -26,14 +38,14 @@ export class CheckoutComponent implements OnInit{
         street: [''],
         city: [''],
         state: [''],
-        coutry: [''],
+        country: [''],
         zipCode: ['']
       }),
       billingAddress: this.formBuilder.group({
         street: [''],
         city: [''],
         state: [''],
-        coutry: [''],
+        country: [''],
         zipCode: ['']
       }),
       creditCard: this.formBuilder.group({
@@ -45,21 +57,106 @@ export class CheckoutComponent implements OnInit{
         expirationYear: ['']
       })
     });
+
+    // populate credit card years
+    this.shopFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+    // populate countries
+    this.shopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieve countries: " +JSON.stringify(data));
+        this.countries = data;
+      }
+    )
+
   }
 
   onSubmit(){
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
+
+    console.log(this.checkoutFormGroup.get('shippingAddress')?.value.country.name)
+    console.log(this.checkoutFormGroup.get('shippingAddress')?.value.state.name)
   }
 
   copyShippingAddressToBillingAddress(event: any){
     if (event.target.checked){
       this.checkoutFormGroup.controls['billingAddress']
         .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+
+      // bug fix code
+      this.billingAddressStates = this.shippingAddressStates;
     }
     else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      // bug fix for states
+      this.billingAddressStates = [];
     }
   }
 
+  handleMonthAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value['expirationYear']);
+
+    // if the current year equals the selected year, then start with current month
+    let startMonth: number;
+
+    if  (currentYear === selectedYear){
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.shopFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress'){
+          this.shippingAddressStates = data;
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup!.value.state = data[0];
+      }
+    );
+  }
 }
